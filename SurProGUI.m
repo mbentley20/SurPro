d5e9d419072b38303909d25586af581ad4b16bf6
Mar <51238705+mbentley20@users.noreply.github.com>
2019-12-13 10:50:45 -0500

    Updates and fixes to the apperance

--- SurProGUI.m
+++ SurProGUI.m
@@ -1,35 +1,35 @@
-function varargout = SurPro(varargin)
-% SURPRO MATLAB code for SurPro.fig
-%      SURPRO, by itself, creates a new SURPRO or raises the existing
+function varargout = SurProGUI(varargin)
+% SURPROGUI MATLAB code for SurProGUI
+%      SURPROGUI, by itself, creates a new SURPROGUI or raises the existing
 %      singleton*.
 %
-%      H = SURPRO returns the handle to a new SURPRO or the handle to
+%      H = SURPROGUI returns the handle to a new SURPROGUI or the handle to
 %      the existing singleton*.
 %
-%      SURPRO('CALLBACK',hObject,eventData,handles,...) calls the local
-%      function named CALLBACK in SURPRO.M with the given input arguments.
+%      SURPROGUI('CALLBACK',hObject,eventData,handles,...) calls the local
+%      function named CALLBACK in SURPROGUI.M with the given input arguments.
 %
-%      SURPRO('Property','Value',...) creates a new SURPRO or raises the
+%      SURPROGUI('Property','Value',...) creates a new SURPROGUI or raises the
 %      existing singleton*.  Starting from the left, property value pairs are
-%      applied to the GUI before SurPro_OpeningFcn gets called.  An
+%      applied to the GUI before SurProGUI_OpeningFcn gets called.  An
 %      unrecognized property name or invalid value makes property application
-%      stop.  All inputs are passed to SurPro_OpeningFcn via varargin.
+%      stop.  All inputs are passed to SurProGUI_OpeningFcn via varargin.
 %
 %      *See GUI Options on GUIDE's Tools menu.  Choose "GUI allows only one
 %      instance to run (singleton)".
 %
 % See also: GUIDE, GUIDATA, GUIHANDLES
 
-% Edit the above text to modify the response to help SurPro
+% Edit the above text to modify the response to help SurProGUI
 
-% Last Modified by GUIDE v2.5 10-Dec-2019 14:43:21
+% Last Modified by GUIDE v2.5 13-Dec-2019 10:36:38
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
                    'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @SurPro_OpeningFcn, ...
-                   'gui_OutputFcn',  @SurPro_OutputFcn, ...
+                   'gui_OpeningFcn', @SurProGUI_OpeningFcn, ...
+                   'gui_OutputFcn',  @SurProGUI_OutputFcn, ...
                    'gui_LayoutFcn',  [] , ...
                    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
@@ -44,15 +44,15 @@ end
 % End initialization code - DO NOT EDIT
 
 
-% --- Executes just before SurPro is made visible.
-function SurPro_OpeningFcn(hObject, eventdata, handles, varargin)
+% --- Executes just before SurProGUI is made visible.
+function SurProGUI_OpeningFcn(hObject, eventdata, handles, varargin)
 % This function has no output args, see OutputFcn.
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
-% varargin   command line arguments to SurPro (see VARARGIN)
+% varargin   command line arguments to SurProGUI (see VARARGIN)
 
-%% Wheel Path 1 Refrence from our SurPro (Date - November 2019)
+%% Wheel Path 1 Refrence from our SurProGUI (Date - November 2019)
 Ref_wheel1=num2cell(repmat(70.41201,12,1));
 
 data_wheel1=get(handles.uitable1,'Data');
@@ -60,7 +60,7 @@ data_wheel1(:,2)=Ref_wheel1;
 set(handles.uitable1,'Data', data_wheel1);
 
 
-%%Wheel Path 2 Refrence from our SurPro ( Date - November 2019)
+%%Wheel Path 2 Refrence from our SurProGUI ( Date - November 2019)
 Ref_wheel2=num2cell(repmat(40.89488,12,1));
 
 data_wheel2=get(handles.uitable2,'Data');
@@ -68,14 +68,14 @@ data_wheel2(:,2)=Ref_wheel2;
 set(handles.uitable2,'Data', data_wheel2);
 
 
-%% Wheel Path 3 Refrence from our SurPro ( Date - November 2019)
+%% Wheel Path 3 Refrence from our SurProGUI ( Date - November 2019)
 Ref_wheel3=num2cell(repmat(93.51557,12,1));
 
 data_wheel3=get(handles.uitable3,'Data');
 data_wheel3(:,2)=Ref_wheel3;
 set(handles.uitable3,'Data', data_wheel3);
 
-%% Wheel Path 4 Refrence from our SurPro ( Date - November 2019)
+%% Wheel Path 4 Refrence from our SurProGUI ( Date - November 2019)
 
 Ref_wheel4=num2cell(repmat(154.687,12,1));
 
@@ -83,20 +83,26 @@ data_wheel4=get(handles.uitable4,'Data');
 data_wheel4(:,2)=Ref_wheel4;
 set(handles.uitable4,'Data', data_wheel4);
 
+% Choose default command line output for SurProGUI
+handles.output = hObject;
 
+% Update handles structure
+guidata(hObject, handles);
 
-% UIWAIT makes SurPro wait for user response (see UIRESUME)
+% UIWAIT makes SurProGUI wait for user response (see UIRESUME)
 % uiwait(handles.figure1);
 
 
 % --- Outputs from this function are returned to the command line.
-function varargout = SurPro_OutputFcn(hObject, eventdata, handles) 
+function varargout = SurProGUI_OutputFcn(hObject, eventdata, handles) 
 % varargout  cell array for returning output args (see VARARGOUT);
 % hObject    handle to figure
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
 
 % Get default command line output from handles structure
+varargout{1} = handles.output;
+
 
 
 % --- Executes during object creation, after setting all properties.
@@ -466,7 +472,7 @@ end
 
 
 % --- Executes on button press in database.
-function database_Callback(hObject, eventdata, handles)
+function database_Callback(~, eventdata, handles)
 % hObject    handle to database (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
@@ -604,3 +610,111 @@ function pushbutton1_ButtonDownFcn(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes when entered data in editable cell(s) in uitable6.
+function uitable6_CellEditCallback(hObject, eventdata, handles)
+% hObject    handle to uitable6 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) edited
+%	PreviousData: previous data for the cell(s) edited
+%	EditData: string(s) entered by the user
+%	NewData: EditData or its converted form set on the Data property. Empty if Data was not changed
+%	Error: error string when failed to convert EditData to appropriate value for Data
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function uitable6_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to uitable6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes during object deletion, before destroying properties.
+function uitable6_DeleteFcn(hObject, eventdata, handles)
+% hObject    handle to uitable6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function uitable6_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to uitable6 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on key press with focus on uitable6 and none of its controls.
+function uitable6_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to uitable6 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Key: name of the key that was pressed, in lower case
+%	Character: character interpretation of the key(s) that was pressed
+%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes when selected cell(s) is changed in uitable7.
+function uitable7_CellSelectionCallback(hObject, eventdata, handles)
+% hObject    handle to uitable7 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) currently selecteds
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function uitable7_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to uitable7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes during object deletion, before destroying properties.
+function uitable7_DeleteFcn(hObject, eventdata, handles)
+% hObject    handle to uitable7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --------------------------------------------------------------------
+function uitable7_ButtonDownFcn(hObject, eventdata, handles)
+% hObject    handle to uitable7 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes on key press with focus on uitable7 and none of its controls.
+function uitable7_KeyPressFcn(hObject, eventdata, handles)
+% hObject    handle to uitable7 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Key: name of the key that was pressed, in lower case
+%	Character: character interpretation of the key(s) that was pressed
+%	Modifier: name(s) of the modifier key(s) (i.e., control, shift) pressed
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes when entered data in editable cell(s) in uitable3.
+function uitable3_CellEditCallback(hObject, eventdata, handles)
+% hObject    handle to uitable3 (see GCBO)
+% eventdata  structure with the following fields (see MATLAB.UI.CONTROL.TABLE)
+%	Indices: row and column indices of the cell(s) edited
+%	PreviousData: previous data for the cell(s) edited
+%	EditData: string(s) entered by the user
+%	NewData: EditData or its converted form set on the Data property. Empty if Data was not changed
+%	Error: error string when failed to convert EditData to appropriate value for Data
+% handles    structure with handles and user data (see GUIDATA)
+
+
+% --- Executes during object creation, after setting all properties.
+function uitable2_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to uitable2 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
+
+
+% --- Executes during object creation, after setting all properties.
+function uitable4_CreateFcn(hObject, eventdata, handles)
+% hObject    handle to uitable4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    empty - handles not created until after all CreateFcns called
